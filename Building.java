dcbba6b93f64d682d7a1f83ebcaa9c45387b07a4
LOW KAH SHENG <lowkahsheng2610@gmail.com>
2026-01-28 11:32:12 +0800

    Add files via upload

--- Building.java
+++ Building.java
@@ -1,30 +1,23 @@
 package Resit_FA_LOWKAHSHENG_SCSJ2500286;
-
 public class Building {
-
 	static String name;
 	static String purpose;
-
 	protected Building (String name, String purpose) {
 	}
-
 	public static void Dairy (String n, String p)
 	{
 		name = n;
 		purpose = p;}
 		
-	public void displayInfo(){
-		
+	public void displayInfo(){	
 		System.out.println("Name:" + name);
 		System.out.println("Purpose:" + purpose);}
 		
 	public static void main(String[]args) {
-
 	Building d = new Building (name = "Library", purpose = "Study Area");
 	Building e = new Building (name = "Canteen", purpose = "Dining Area");
 	d.displayInfo();
 	e.displayInfo();
-	}
-	
+	}	
 }
 
